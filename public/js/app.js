5958023f7c159d52602ac15671d388ae85215849
Rachels Mac <rasilverthorn@ucdavis.edu>
2020-01-18 10:23:15 -0800

    pushing

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,14 @@ $(document).ready(function () {
     $('.sidenav').sidenav();
 });
 
+
+
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+});
+
+
+// $(document).ready(function () {
+//     $('.carousel').carousel();
+// });
